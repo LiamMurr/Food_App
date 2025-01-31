f1e8b80d510cd6bba8830bc6a5e8f4fa9d441240
Liam <murray.liam5426@gmail.com>
2025-02-01 12:39:59 +1300

    Insert test data and connect mongo

---
+++ models/restaurant.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const restaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  cuisine: { type: String, required: true },
+  location: { type: String, required: true },
+  coordinates: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  allergy_friendly: { type: [String], required: true },
+  price: { type: String, enum: ["$", "$$", "$$$"], required: true },
+});
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+
+module.exports = Restaurant;
---
+++ seeds/seeds.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+const Restaurant = require("../models/restaurant.js"); // Import Mongoose schema
+const testData = require('./test-data.json');
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/food-finder")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+
+console.log(testData);
+
+// Insert data into MongoDB
+// Restaurant.insertMany(testData)
+//   .then(() => {
+//     console.log("Restaurants added successfully!");
+//     mongoose.connection.close(); // Close connection after insert
+//   })
+//   .catch((err) => console.error(err));
