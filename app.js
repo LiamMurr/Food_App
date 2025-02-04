c35956a0f46fe159030ee192763699d5f54b2074
Liam <murray.liam5426@gmail.com>
2025-02-05 11:22:11 +1300

    Sort mongo bug

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
-const express = require('express');
-const path = require("path");
-const mongoose = require("mongoose");
-const ejsMate = require('ejs-mate');
-// const fs = require("fs");
+const express = require("express")
+const path = require("path")
+const mongoose = require("mongoose")
+const ejsMate = require("ejs-mate")
 
+const homeRoutes = require("./routes/home")
 
-const homeRoutes = require('./routes/home');
-const restaurantsRoutes = require('./routes/restaurants')
+mongoose
+  .connect("mongodb://127.0.0.1:27017/food-finder")
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Database connection error:", err))
 
-mongoose.connect('mongodb://127.0.0.1:27017/food-finder');
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
-});
+const app = express()
 
-const app = express();
+app.engine("ejs", ejsMate)
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/", homeRoutes)
 
-app.use('/', homeRoutes);
-// app.use('/', restaurantsRoutes) Second router for restaurant page?
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, that route doesn't exist.")
+})
 
+// Error handler
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    
-    if (err.name === "ValidationError") {
-        console.log(err.name);
-        return res.status(statusCode).render("error", { err });
-    }
-    
-    if (err.name === "CastError") {
-        console.log(err.name);
-        return res.status(statusCode).render("error", { err });
-    }
-    
-    res.status(statusCode).render("error", { err });
-});
+  console.error(err.stack)
+  res.status(500).send("Something broke! Error: " + err.message)
+})
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000');
+  console.log("Serving on port 3000")
 })
--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Restaurants = require('../models/restaurant.js')
 
 
 router.get('/', (req, res) => {
     res.render('restaurants/home')
 });
 
-router.get('/restaurants', (req, res) => {
-    res.render('restaurants/show')
+router.get('/restaurants', async (req, res) => {
+    const restaurant = await Restaurants.findOne({ name : "Middle Eastern Magic" });
+    console.log(restaurant.name);
+    res.render('restaurants/show', { restaurant })
 });
 
 
